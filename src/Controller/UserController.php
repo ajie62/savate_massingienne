a3c153e5cb604ffde94dff9961e51d0b05a9657c
ajie62 <buteljerome@yahoo.fr>
2018-02-05 02:56:09 +0100

    User - Added new role_hierarchy in security.yaml - Updated User Entity with new attributes - Updated AdminController & UserController methods that are related with User - Removed useless views
    A user can update his profile. An admin can update users' profile.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,35 +8,68 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Service\UserService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user.index")
+     * @Route("/list", name="user.list")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function list()
     {
-        return $this->render('user/index.html.twig');
+        return $this->render('user/list.html.twig');
     }
 
     /**
-     * @Route("/user/{username}", name="user.read")
+     * @Route("/profile/{username}", name="user.profile", defaults={"username" = null})
+     * @param $username
+     * @param UserService $userService
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function read()
+    public function profile($username, UserService $userService)
     {
-        return $this->render('user/read.html.twig');
+        if ($username == $this->getUser()->getUsername()) {
+            return $this->redirectToRoute('user.profile', ['username' => null]);
+        }
+        $self = false;
+        $user = $userService->getUserByUsername($username, $self);
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+            'self' => $self
+        ]);
     }
 
     /**
-     * @Route("/user/{id}/update", name="user.update")
+     * @Route("/user/update", name="user.update")
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function update()
+    public function update(Request $request)
     {
-        return $this->render('user/update.html.twig');
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('user.profile', ['username' => $user->getUsername()]);
+        }
+
+        return $this->render('user/update.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
     }
 }
