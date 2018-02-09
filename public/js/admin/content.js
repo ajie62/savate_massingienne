0a8a1aab0293b3147a964fe4ec44bea022ffc8b8
ajie62 <buteljerome@yahoo.fr>
2018-02-09 21:49:53 +0100

    Fixing add team member issue in content.js

--- public/js/admin/content.js
+++ public/js/admin/content.js
@@ -3,12 +3,12 @@ $(function() {
     // Add
     var $addTeamMemberButton = $('#association_addTeamMemberButton');
     var $teamMembersSubformsContainer = $('#association_teamMembers');
-    var prototype = $teamMembersSubformsContainer.data('prototype');
+    var dataPrototype = $teamMembersSubformsContainer.data('prototype');
     var nbAdded = 0;
 
     $addTeamMemberButton.on('click', function() {
         nbAdded++;
-        prototype = prototype.replace(/__name__/g, 'form-' + nbAdded);
+        var prototype = dataPrototype.replace(/__name__/g, 'form-' + nbAdded);
         $teamMembersSubformsContainer.append(prototype);
     });
 
