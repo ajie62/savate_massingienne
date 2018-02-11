e233e87ae4d68c9665094652e20b122154848de2
ajie62 <buteljerome@yahoo.fr>
2018-02-11 21:30:49 +0100

    Contact: ORM & Constraints

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -8,35 +8,79 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="contact")
+ */
 class Contact
 {
     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
      * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="Ce champ ne doit pas être vide."
+     * )
      */
     private $firstname;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="Ce champ ne doit pas être vide."
+     * )
      */
     private $lastname;
 
     /**
      * @var string
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *     message="Veuillez s'il vous plaît utiliser une adresse email valide."
+     * )
      */
     private $email;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank(
+     *     message="Ce champ ne doit pas être vide."
+     * )
+     * @Assert\Length(
+     *     max="200",
+     *     maxMessage="Ce champ ne peut contenir que {{ limit }} caractères maximum."
+     * )
      */
     private $subject;
 
     /**
      * @var string
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(
+     *     message="Ce champ ne doit pas être vide."
+     * )
+     * @Assert\Length(
+     *     max="1000",
+     *     maxMessage="Ce champ ne peut contenir que {{ limit }} caractères maximum."
+     * )
      */
     private $content;
 
     /**
      * @var \DateTime
+     * @ORM\Column(type="datetime")
      */
     private $sendAt;
 
@@ -46,6 +90,24 @@ class Contact
     public function __construct()
     {
         $this->sendAt = new \DateTime();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Contact
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
     }
 
     /**
