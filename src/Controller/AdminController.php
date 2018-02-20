1027c81487603eb65a561feb741710bc2b998143
ajie62 <buteljerome@yahoo.fr>
2018-02-20 12:46:22 +0100

    Styles - Admin 'news' styles - Minor changes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -734,6 +734,8 @@ class AdminController extends AbstractController
             $this->em->remove($news);
             # Flush
             $this->em->flush();
+            # Flash message
+            $this->addFlash('notice', 'L\'actualité a bien été supprimée.');
 
             # Redirection to admin.news
             return $this->redirectToRoute('admin.news');
@@ -760,21 +762,31 @@ class AdminController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            # If news is being updated, set the updatedAt attr
             if (!$isNewNews) {
                 $news->setUpdatedAt(new \DateTime());
             }
 
+            # Persist
             $this->em->persist($news);
+            # Flush
             $this->em->flush();
 
-            return $this->redirectToRoute('admin.news_read', [
-                'id' => $news->getId(),
-            ]);
+            # Flash message
+            if ($isNewNews) {
+                $this->addFlash('notice', 'L\'actualité a été publiée.');
+            } else {
+                $this->addFlash('notice', 'L\'actualité a bien été modifiée.');
+            }
+
+            # Redirection to admin.news
+            return $this->redirectToRoute('admin.news');
         }
 
         return $this->render('admin/news/set.html.twig', [
             'form' => $form->createView(),
             'isNewNews' => $isNewNews,
+            'news' => $news
         ]);
     }
 
